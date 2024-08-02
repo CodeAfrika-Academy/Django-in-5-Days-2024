985d4c674e80604cea8e749b23f42a6a08f44d5f
engenovic <engenovic@gmail.com>
2024-08-02 19:34:06 +0300

    Modifed Student Admin display on admin

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Student
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['regno','first_name','last_name','email','phone']
+    list_filter = ['first_name']
+    search_fields = ['regno','first_name','email']
+    ordering = ['first_name']
+    show_facets = admin.ShowFacets.ALWAYS
--- students/models.py
+++ students/models.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    regno = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
 
 
-
-
-
-
-
+    
