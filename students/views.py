e6bf20ed3c36e956f8ea3e76eea2bc408336e0c8
engenovic <engenovic@gmail.com>
2024-08-02 21:28:11 +0300

    Implemented Django views and templates

--- students/views.py
+++ students/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Student 
 
-# Create your views here.
+# Display a list of all students
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'student/list.html',{'students':students})
+
+
+# Display a single student
+
+def student_detail(request,id):
+    student = Student.objects.get(id=id)
+
+    return render(request, 'student/detail.html',{'student':student})
+    
